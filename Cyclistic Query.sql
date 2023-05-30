5b6f31dc5087f768ad0eb8876f9974beb3931b1f
Samuel Effiom <89613623+sammytrace@users.noreply.github.com>
2023-05-30 19:50:05 +0100

    Update Cyclistic Query.sql
    Added some more description to the project

--- Cyclistic Query.sql
+++ Cyclistic Query.sql
@@ -1,4 +1,48 @@
--- Merging data from Dec 2021 to Nov 2022 into one dataset
+-- CASE STUDY: HOW DOES A BIKE-SHARE NAVIGATE SPEEDY SUCCESS?
+
+-- TABLE OF CONTENT
+
+-- Introduction
+-- Exploratory Analysis
+-- Conclusion
+
+/* INTRODUCTION
+
+Cyclistic is an imaginary bike-sharing company that started out in 2016 and has grown its fleet to about 5,824 bicycles. Until now, Cyclistic's marketing strategy relied on 
+building general awareness and appealing to broad consumer segments. One approach that helped make these things possible was the flexibility of its pricing plans: single-ride
+passes, full-day passes, and annual memberships. Customers who pursue single-ride or full-day passes are referred  to as casual riders. Customers who purchase annual memberships
+are called cyclistic members.
+The Director of Marketing believes the company's future success depends on maximizing the number of annual memberships. Therefore, your team wants to understand how casual 
+riders and annual members use cyclistic bikes differently and this will form the basis of our analysis.*/
+
+/* ABOUT THE DATASET
+This dataset features twelve months of data from December 2021 to November 2022 all stored in excel sheets per month and months data has 13 columns showing different attributes
+of the data. This dataset was retrieved from Cousera as it forms part of the portfolio case study for the Google Data Analytics certificate.*/
+
+-- OBJECTIVES
+/* The primary objectives of this data analysis project are:
+1) Discover how annual members and casual riders use cyclistic bikes differently.
+2) Give Top 3 recommendations on how the company can influence casual riders to purchase annual memberships.*/
+
+-- METHODOLOGY
+/* 
+WITH EXCEL
+1) Data Cleaning: Remove any duplicates, missing values, or irrelevant columns from each of the dataset to be done with excel.
+2) Conduct descriptive analysis, calculate mean of ride length, maximum, mode of day of week.
+3) Create Pivot tables to quickly calculate and visualize the data.
+
+WITH SQL
+4) Merge all of the cleaned data into one table to use for analysis.
+5) Investigate trends on data to gain insights on the objectives of this project.
+
+WITH TABLEAU
+6) Create effective data visualization to present your findings.
+7) Give your Top 3 recommendations on how the company can influence casual riders to purchase annual memberships.
+*/
+
+-- EXPLORATORY ANALYSIS
+
+-- Merging 12 months of data from Dec 2021 to Nov 2022 into one dataset
 
 SELECT ride_id,
 	rideable_type AS bike_type,
@@ -179,7 +223,9 @@ SELECT ride_id,
 	day_of_week
 FROM [dbo].[Nov_2022] 
 
--- Create a new table
+-- On merging the dataset, we how have 5,733,451 rows and 12 columns in the dataset. 
+
+-- Create a new table and insert into it the merged dataset
 
 	CREATE TABLE Bike_Data (
 		ride_id nvarchar(225),
@@ -196,7 +242,7 @@ FROM [dbo].[Nov_2022]
 		day_of_week float
 	);
 
--- Inserting merged data into new table
+-- Inserting merged data into new table Bike_Data
 INSERT INTO Bike_Data
 SELECT ride_id,
 	rideable_type AS bike_type,
@@ -377,11 +423,11 @@ SELECT ride_id,
 	day_of_week
 FROM [dbo].[Nov_2022]
 
--- TO GET A LOOK AT THE NEWLY MERGED DATASET
+-- ASSESSING THE NEWLY MERGED DATASET
 SELECT *
 FROM Bike_Data 
 
---TO GET THE COUNT OF RIDES FOR THE ENTIRE DATASET GROUPED BY THE DIFFERENT RIDER_CLASS
+--TO GET THE COUNT OF RIDES FOR THE ENTIRE DATASET GROUPED BY THE DIFFERENT RIDER_CLASSES
 SELECT rider_class,
 	COUNT(*) as sum_of_rides
 FROM Bike_Data
@@ -394,7 +440,7 @@ SELECT bike_type,
 FROM Bike_Data
 GROUP BY bike_type,
 	rider_class
-ORDER BY bike_type 
+ORDER BY sum_of_rides DESC 
 
 -- TO GET SUM OF RIDES BY RIDER_CLASS FOR THE DIFFERENT DAYS OF THE WEEK
 SELECT rider_class,
@@ -494,3 +540,4 @@ FROM Bike_Data
 GROUP BY rider_class,
 	day_of_week
 ORDER BY day_of_week
+/* The tables created from the views in this analysis and the top3 recommendations on how the convert casual riders to annual members will be made with be made on Tableau */
